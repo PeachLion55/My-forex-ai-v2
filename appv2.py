849839584c5d997a1524f13b410429eb131bbff4
PeachLion55 <peachlion55@sudomail.com>
2025-08-24 14:00:18 +0100

    Update appv2.py

--- appv2.py
+++ appv2.py
@@ -2305,7 +2305,7 @@ elif st.session_state.current_page == "Zenvo Academy":
         if 'logged_in_user' in st.session_state:
             del st.session_state.logged_in_user
         st.session_state.drawings = {}
-        st.session_state.tools_st.rerunurnal = pd.DataFrame(columns=journal_cols).astype(journal_dtypes)
+        st.session_state.tools_trade_journal = pd.DataFrame(columns=journal_cols).astype(journal_dtypes)
         st.session_state.strategies = pd.DataFrame(columns=["Name", "Description", "Entry Rules", "Exit Rules", "Risk Management", "Date Added"])
         st.session_state.emotion_log = pd.DataFrame(columns=["Date", "Emotion", "Notes"])
         st.session_state.reflection_log = pd.DataFrame(columns=["Date", "Reflection"])
