91cc0ef72cd9137f5170054e894f6f23c83ce1c6
PeachLion55 <peachlion55@sudomail.com>
2025-08-24 15:59:28 +0100

    Update appv2.py

--- appv2.py
+++ appv2.py
@@ -1791,6 +1791,9 @@ elif st.session_state.current_page == 'account':
                 st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
                 
             with insights_col:
+                # Add a wrapper div with left padding to create space from the chart column
+                st.markdown("<div style='padding-left: 30px;'>", unsafe_allow_html=True)
+                
                 st.markdown("<h5 style='text-align: center;'>Personalized Insights</h5>", unsafe_allow_html=True)
                 
                 insight_message = ""
@@ -1813,6 +1816,9 @@ elif st.session_state.current_page == 'account':
                      next_milestone = "The next streak badge is at 30 days. You've got this!"
 
                 st.markdown(f"<div class='insights-card' style='margin-top: 10px;'><p>🎯 **Next Up:** {next_milestone}</p></div>", unsafe_allow_html=True)
+
+                st.markdown("</div>", unsafe_allow_html=True)
+
 
         # --- Row 3: XP Journey Chart (This part goes right after the `with col1:` and `with col2:` blocks) ---
         st.markdown("<hr style='border-color: #4d7171;'>", unsafe_allow_html=True)
