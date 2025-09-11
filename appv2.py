132e9ad48cc27f5fcce387c641dcd341b003d807
PeachLion55 <peachlion55@sudomail.com>
2025-09-11 18:39:15 +0100

    Update appv2.py

--- appv2.py
+++ appv2.py
@@ -905,7 +905,7 @@ try:
     logo_str = base64.b64encode(buffered.getvalue()).decode()
     st.sidebar.markdown(
         f"""
-        <div style='text-align: center; margin-bottom: 20px;'>
+        <div style='text-align: center; margin-bottom: 30px;'>
             <img src="data:image/png;base64,{logo_str}" width="60" height="50"/>
         </div>
         """,
